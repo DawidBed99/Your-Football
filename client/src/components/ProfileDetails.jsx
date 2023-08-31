a6421ac0250c39228f1fa83b17a89c313715ef38
Dawid Bednarz <160144@stud.prz.edu.pl>
2023-08-31 18:02:53 +0200

    Modal on fullscreen

--- client/src/components/ProfileDetails.jsx
+++ client/src/components/ProfileDetails.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import { Alert, Avatar, Box, Divider, Paper, Stack, TextField, Typography } from "@mui/material";
+import { Alert, Avatar, Box, Divider, Modal, Paper, Stack, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./MScomponents/NavBar";
 import LeftBar from "./MScomponents/LeftBar";
 import "./profileImg.css"
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'rgba(0,0,0,0.7)',
+};
 
 function ProfileDetails() {
 
@@ -19,7 +27,6 @@ const handleClose = (e) =>{
 
 }
 
-  
   return (
     <Box height="100vh">
     <NavBar  />
@@ -41,13 +48,25 @@ const handleClose = (e) =>{
         <Typography sx={{variant:{xs:"h4", sm:"h3"}}} ml="6px">Messi</Typography>
         </Stack>
         </Stack>
+
+        <Modal
+        disableAutoFocus={true}
+        open={fullScreen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+      <Box sx={style}>
+        <img className="profImg" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg" />
       </Box>
-      <Box onClick={handleClose} bgcolor="rgba(0,0,0,0.7)" height="90.5%"  width="100%"  position="absolute" sx={{display:fullScreen ? "": "none" }}  >
+     </Modal>
+      </Box>
+      {/* <Box onClick={handleClose} bgcolor="rgba(0,0,0,0.7)" height="90.5%"  width="100%"  position="absolute" sx={{display:fullScreen ? "": "none" }}  >
       <Stack 
        sx={{mt:"20px",width:"100%", alignItems:"center"}}>
         <img className="profImg" style={{ display:fullScreen ? "" :"none", position:"fixed"}} src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg"></img>
         </Stack>
-        </Box>
+        </Box> */}
     </Stack>
    
   </Box>
